775b663d33033e1b9a0753c5bc04093f27354612
rashmiap <rashmiap.10@gmail.com>
2018-11-09 12:01:33 +0530

    timestamp api added /api/timestamp/:date

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,11 +19,27 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//timestamp API endpoint...
+app.get("/api/timestamp/:date_string", function (req, res) {
+  let time_stamp = (req.params.date_string) ? new Date(req.params.date_string) : new Date();
+  if(time_stamp.toString() === 'Invalid Date' && Date.parse(parseInt(req.params.date_string)) !== NaN){
+    time_stamp = new Date(parseInt(req.params.date_string));
+  }
+  else if(time_stamp.toString() === 'Invalid Date'){
+    res.json({error: 'Invalid Date'})
+    return res.end();
+  }
+  res.json({
+    unix: time_stamp.getTime(),
+    utc: time_stamp.toUTCString()
+  })
+  return res.end();
+})
 
 
 // listen for requests :)
