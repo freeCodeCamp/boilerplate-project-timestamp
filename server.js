d9e3a995e5f4ecda59f69d14163c59bd6b4f8159
Steve Golden <sgolden2525@outlook.com>
2019-03-24 01:40:25 -0500

    route functionality
    Change route path
    
    Added route functionality so when route path and date information is entered it will return the unix and full date/time format

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:date_string", function (req, res) {
+  const date = new Date(req.params.date_string)
+  res.json({ "unix": date.getTime(), "utc": date.toUTCString() });
 });
 
 
