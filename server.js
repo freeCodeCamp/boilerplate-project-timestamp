ff0ed642d52229b2e53a2bbbcdfb86eab439a909
Glitch (polarized-sudden-acoustic) <none>
2020-05-06 20:40:07 +0000

    🚨👋 Updated with Glitch

--- server.js
+++ server.js
@@ -30,3 +30,29 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get("/api/timestamp/:dateString?", (req, res) => {
+  const dateString = req.params.dateString;
+  
+  let date;
+  
+  //If the date string is empty it should be equivalent to trigger new Date(); and use current timestamp.
+  if (!dateString) {
+    date = new Date();
+  } else {
+    //If the date string is a number convert the string to an integer and make a date from the integer.
+    if(!isNaN(dateString)) {
+      date = new Date(parseInt(dateString));
+    } else {
+      //Make a date from the string.
+      date = new Date(dateString);
+    }
+  }
+  //If the date string is invalid return a JSON with object {"error": "Invalid Date"}.
+  if (date.toString() === "Invalid Date") {
+    res.json({ error: date.toString() });
+  } else {
+    //If the date string is valid return a JSON object {"unix": <date.getTime()>, "utc": <date.toUTCString()>}
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
+});
