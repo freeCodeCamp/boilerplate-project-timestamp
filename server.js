30d5fb799e50b68dd0435946959b1f68af1f5167
Soumitra Varindra Chakravarti <soumitra.chakravarti@gmail.com>
2020-05-23 14:42:25 -0700

    Complete timestamp API
    for freecodecamp

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,12 +18,42 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// API TimeStamp Endpoint
+app.get("/api/timestamp/:dateString?", (request, response) => {
+  const dateString = request.params.dateString;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  let date;
+
+  // Empty dateString, response with current time
+  if (!dateString) {
+    date = new Date();
+  } else {
+    // dateString is not empty, parse and convert if integer
+    if (!isNaN(dateString)) {
+      date = new Date(parseInt(dateString));
+    } else {
+      // dateString is good as it is
+      date = new Date(dateString);
+    }
+  }
+  // Invalid dateString using standard date class, respond with error
+  if (date.toString() === "Invalid Date") {
+    response.json(
+      {
+        error: date.toString()
+      }
+    );
+  } else {
+    // Ok Response
+    response.json(
+      {
+        unix: date.getTime(),
+        UTC: date.toUTCString()
+      }
+    );
+  }
+
 });
-
 
 
 // listen for requests :)
