72e554a6af443aca1bb52c007759319e70c789f1
Glitch (fcc-time-stamp-) <none>
2021-05-08 22:45:33 +0000

    🎀🐑 Updated with Glitch - Complete

--- server.js
+++ server.js
@@ -18,6 +18,44 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//Function to check if date is valid
+let isValidDate = function(d) {
+  return d instanceof Date && !isNaN(d);
+};
+
+app.route('/api/:date?').get(function(req, res){
+  let input = req.params.date;
+  let responseObject = {};
+  
+  //An empty date parameter should return the current time in a JSON object with a unix and UTC key
+  if (input === undefined){
+    let date = new Date();
+    responseObject['unix']= Math.round(date);
+    responseObject['utc']= date.toUTCString();
+  }
+  else {
+    // If the input date string is invalid, the api returns an object having the structure 
+    // { error : "Invalid Date" }
+    if(!isValidDate(new Date(input)) && !isValidDate(new Date(parseInt(input)))) {
+      responseObject['error'] = "Invalid Date";
+    } 
+    //Date is valid
+    else {
+      let date;
+      //Date is valid but date is in unix format
+      if(!isValidDate(new Date(input))){
+        date = new Date(parseInt(input));
+      } 
+      //Date is valid but is in non-unix format
+      else {
+        date = new Date(input);  
+      }
+      responseObject['unix']= Math.round(date);
+      responseObject['utc']= date.toUTCString();
+    }
+  }
+  res.json(responseObject);
+});
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
