c5dc6e7d3944ad29be806172bedef26f1a729d72
Aglazer <51724278+aviglazer1998@users.noreply.github.com>
2021-12-08 14:22:52 -0500

    added to git

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
-// server.js
-// where your node app starts
-
-// init project
-var express = require('express');
+require("dotenv").config();
+var express = require("express");
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+	res.json({ greeting: "hello API" });
 });
 
+app.get("/api/whoami", (req, res) => {
+	res.json({
+		ipaddress: req.headers["x-forwarded-for"],
+		language: req.headers["accept-language"],
+		software: req.headers["user-agent"],
+	});
+});
 
-
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+	console.log("Your app is listening on port " + listener.address().port);
 });
