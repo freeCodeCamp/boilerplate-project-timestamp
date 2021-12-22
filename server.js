1332b559d8d6ff9160c090a331af4bbdee6874a3
Dhakshin Krishna <dozerex007@gmail.com>
2021-12-22 17:07:37 +0530

    completed all test cases passed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // where your node app starts
 
 // init project
+require("dotenv").config();
 var express = require('express');
 var app = express();
 
@@ -18,10 +19,37 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
+const days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+const months=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:word?/", function (req, res) {
+  var str=req.params.word;
+  console.log(str);
+  if(str==null){
+    str=new Date();
+  }
+  else if(str.includes('-')||str.includes(' ')){
+    console.log("will remain as a str "+str);
+  }
+  else{
+    str=parseInt(str);
+  }
+  if(new Date(str)=="Invalid Date"||typeof(str)=='undefined'){
+    res.send({error:"Invalid Date"});
+  }
+  const strUni=new Date(str);
+  const strUnix=strUni.getTime();
+  var strDay=strUni.getDay();
+  var month=strUni.getMonth();
+  var strUtcDay=days[strDay]+", ";
+  strUtcDay+=('0' + strUni.getDate()).slice(-2)+" ";
+  strUtcDay+=months[month]+" ";
+  strUtcDay+=strUni.getFullYear()+" ";
+  strUtcDay+=('0' + strUni.getUTCHours()).slice(-2)+":";  
+  strUtcDay+=('0' + strUni.getUTCMinutes()).slice(-2)+":"; 
+  strUtcDay+=('0' + strUni.getUTCSeconds()).slice(-2)+" GMT";  
+  res.send({unix : strUnix , utc:strUtcDay});
+  
 });
 
 
