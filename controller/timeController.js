fa6e2f4b615a415f9691d315b7e70f1c8e273c3e
= <=>
2022-05-12 12:50:28 +0700

    fix error

--- controller/timeController.js
+++
@@ -1,7 +0,0 @@
-const timeStamp = (req, res) => {
-    const unix = 1652281179197;
-    const utc ='Wed, 11 May 2022 15:00:37 GMT';
-    res.status(200).json({"unix": unix, "utc": utc});
-}
-
-exports.timeStamp = timeStamp;
--- index.js
+++ index.js
@@ -2,33 +2,63 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
-const { route } = require('./routes/timeRoute.js');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-app.use(route);
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: req.params.hello });
 });
 
-
+app.get("/api/:stamp", (req, res) => {
+  const stamp = req.params.stamp;
+  if (isNaN(stamp)) {
+    if (new Date(stamp).toUTCString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    } else {
+      res.json({
+        unix: new Date(stamp).getTime(),
+        utc: new Date(stamp).toUTCString(),
+      });
+    }
+  } else {
+    if (new Date(parseInt(stamp)).toUTCString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    } else {
+      res.json({
+        unix: parseInt(stamp),
+        utc: new Date(parseInt(stamp)).toUTCString(),
+    });
+    }
+  }
+  // try {
+  //   if (isNaN(req.params.stamp)) {
+  //     stamp = parseInt(req.params.stamp);
+  //     res.json({ unix: stamp, utc: new Date(stamp).toUTCString() });
+  //   }
+  // } catch (err) {
+  //   console.log(err);
+  // }
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+// var listener = app.listen(process.env.PORT, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port 3000");
 });
