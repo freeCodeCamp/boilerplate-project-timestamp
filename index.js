00a8b23c84d3badf7b25d9bd4c7a4de5b8bb807b
Joshua Balogun <joshplexus8@gmail.com>
2022-06-26 20:10:30 +0100

    commit url

--- index.js
+++ index.js
@@ -2,31 +2,42 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/hello", (req, res) => {
+  res.send({ Unix: "hello API" });
 });
 
+//first endpoint url
+app.get("/api/:dateString", (req, res) => {
+  const dateObject = req.params.dateString;
+  res.json({
+    unix: req.params.dateString,
+    utc: new Date().toUTCString(),
+  });
+});
 
+// const tunix = Date.now();
+// const dateObject = new Date();
+
+// console.log(dateObject.toUTCString());
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
