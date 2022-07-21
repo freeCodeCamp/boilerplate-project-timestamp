7c6fa9b3b3a25f82730d81562971fc0ec4613057
Azl23635 <rmitanzel@gmail.com>
2022-07-21 20:08:37 +1000

    Completed project

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 // where your node app starts
 
 // init project
+
 var express = require('express');
 var app = express();
+
+require('dotenv').config();
+
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -12,6 +16,11 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+
+
+
+
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -25,8 +34,21 @@ app.get("/api/hello", function (req, res) {
 });
 
 
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.use('/api/:date', (req, res) => {
+  let dateInput = req.params.date;
+  
+  (dateInput == parseInt(dateInput).toString()) ? (dateInput = parseInt(dateInput)) : null;
+  let date = (new Date(dateInput)).toUTCString();
+  
+  if (date == 'Invalid Date') {
+    res.json({'error': "Invalid Date"});
+  }
+  else {
+    res.json({'unix':`${Date.parse(date)}`, 'utc':`${date}`});
+  }
+})
