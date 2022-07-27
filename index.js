fabcb7ea1bb653e87e4f609d2bd4cfa5ea51b752
ParadoxPD <ask.priyanshu@gmail.com>
2022-07-27 12:37:53 +0530

    Added the endpoint

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-// index.js
-// where your node app starts
-
-// init project
 const express = require('express');
 const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 const cors = require('cors');
 app.use(cors({
   optionsSuccessStatus: 200
 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
