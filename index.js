58c794ac95d51bd0a3addaff9a07da9507ede999
Rahul Kumar <33459576+RahulkumarRV@users.noreply.github.com>
2022-10-15 13:49:35 +0000

    /api updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/api", function (req, res) {
   let date = new Date();
 
   res.json({
