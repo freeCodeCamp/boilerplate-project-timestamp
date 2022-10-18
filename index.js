7b31b69df2f01ec2798d3fe67da42ab835905443
dannyrae <emmanueldan0920@gmail.com>
2022-10-18 03:18:05 -0700

    timestamp api successfully created and working perfectly

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
@@ -13,13 +9,11 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
