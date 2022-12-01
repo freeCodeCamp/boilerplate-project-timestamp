3e66baa86f60a2f6ed0cd9fa0cf4e0b7a4882f98
Sudhir <sudirsha02@example.com>
2022-12-01 18:14:05 +0530

    initial commit added functionalities but not in proper format

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+const moment  =  require('moment');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -12,6 +13,9 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+
+
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -23,6 +27,60 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+const validate  =  (date)=>{
+  console.log("Testing date ",date);
+  try {
+    let d=new Date(date);
+    if(d){
+      console.log("valid ",d);
+      // console.log()
+    }
+  } catch (error) {
+    console.log("Invalid",d);
+  }
+}
+
+app.get("/api/:date", function (req, res) {
+  let date  =  req.params.date;
+  
+  let result  =  {}
+  if(date.includes('-')){
+    let isValid  =  moment(date, "YYYY-MM-DD").isValid();
+   if(isValid){
+      let unix = moment(date,"YYYY-MM-DD").unix();
+      let utc  =  moment(date).utc().format("ddd, DD MMM YYYY 00:00:00")+" GMT";
+
+      validate(unix);
+      validate(utc);
+      result  =  {
+        "unix"  :  unix,
+        "utc"   :  utc
+      }
+   }else{
+      result  =  { error : "Invalid Date" }
+
+   }
+  }else{
+     try {
+       let unixDate =  moment(parseInt(date)).format('YYYY-MM-DD');
+      let utc  =  moment(unixDate).utc().format("ddd, DD MMMM YYYY 00:00:00")+" GMT";
+      validate(utc) ;
+       
+       result  =  {
+          unix:date,
+          utc:utc
+        }
+       
+     } catch (error) {
+       console.log(error)
+       result  =  { error : "Invalid Date" }
+     }
+  }
+  res.json(result);
+});
+
+
 
 
 
