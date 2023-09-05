9acf0aeaaf6cf2758ba9cf161a0cf02213e36c36
dezaerus <adtow187@gmail.com>
2023-09-05 17:09:55 +0300

    finished the task

--- index.js
+++ index.js
@@ -24,6 +24,30 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// Define a new route for handling date requests
+app.get("/api/:date?", (req, res) => {
+  // Get the date parameter from the request
+  const dateParam = req.params.date;
+
+  // Check if the dateParam is empty or not provided
+  if (!dateParam) {
+    // If dateParam is empty, return the current time
+    const currentDate = new Date();
+    res.json({ unix: currentDate.getTime(), utc: currentDate.toUTCString() });
+  } else {
+    // Try to parse the provided date parameter
+    const parsedDate = new Date(dateParam);
+
+    // Check if the parsed date is valid
+    if (!isNaN(parsedDate.getTime())) {
+      // If valid, return the Unix timestamp and UTC string
+      res.json({ unix: parsedDate.getTime(), utc: parsedDate.toUTCString() });
+    } else {
+      // If invalid, return an error message
+      res.json({ error: "Invalid Date" });
+    }
+  }
+});
 
 
 // listen for requests :)
