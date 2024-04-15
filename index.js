cfdb69fc96b13268ed4d74b5369d45a11f4af07d
Yassine ELAOUNI <80796018+Yassine-deepth@users.noreply.github.com>
2024-04-15 21:39:11 +0100

    Timestamp Microservice | freeCodeCamp.org

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// index.js
-// where your node app starts
-
 // init project
 var express = require('express');
 var app = express();
@@ -8,7 +5,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,15 +15,38 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
+// Your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/:date?", (req, res) => {
+  let date = req.params.date;
 
+  if (!date) {
+    // If the date parameter is empty, return the current time
+    let dateObject = new Date();
+    res.json({ unix: dateObject.getTime(), utc: dateObject.toUTCString() });
+  } else {
+    // A 4 digit number is a valid ISO-8601 for the beginning of that year
+    // 5 digits or more must be a unix time, until we reach a year 10,000 problem
+    if (/\d{5,}/.test(date)) {
+      let dateInt = parseInt(date);
+      // Date regards numbers as unix timestamps, strings are processed differently
+      res.json({ unix: date, utc: new Date(dateInt).toUTCString() });
+    } else {
+      let dateObject = new Date(date);
 
-// Listen on port set in environment variable or default to 3000
+      if (dateObject.toString() === "Invalid Date") {
+        res.json({ error: "Invalid Date" });
+      } else {
+        res.json({ unix: dateObject.getTime(), utc: dateObject.toUTCString() });
+      }
+    }
+  }
+});
+
+// Listen on the port set in the environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
